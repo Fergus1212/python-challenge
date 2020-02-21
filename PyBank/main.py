ffd7f854210163e8c5a17da143404b77fe345d81
Jack Ferguson <jackferg12345@gmail.com>
2020-02-21 15:32:22 -0600

    Assignment 1

---
+++ PyBank/main.py
@@ -0,0 +1,42 @@
+import os
+import csv
+
+csvpath = os.path.join('budget_data.csv')
+
+countv=0
+plsum = 0
+row = [1]
+pl=[]
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header =next(csvreader)
+    rows =[[row[0], int(row[1])] for row in csvreader]
+for row in rows:
+        date= row[0]
+        pl.append(row[1])
+    #data = list(int(rows[1]))
+    #datam = max(data)
+    #print(datam)
+for row in rows:
+    countv = countv+1
+    plsum += int(row[1])
+for row in rows:
+    avgpl = plsum/len(row[0])
+    if row[1] == max(pl):
+        q4 = f"{row[0]} {row[1]}"
+    if row[1] == min(pl):
+        q5 = f"{row[0]} {row[1]}"
+        #if row[1]> row[1]-1
+        #maxp = row[1]
+print("Financial Analysis")
+print("-----------------------------")
+print(f"total months:  {countv}")
+print(f"total:  {plsum}")
+print(f"Average Change:  {avgpl}")
+print(f"Greatest Increase in Profits:   {q4}")
+print(f"Greatest Decrease in Profits:  {q5}")
+Financial_Analysis = open("Financial_Analysis.txt","w")
+Financial_Analysis.write("Financial Analysis\n")
+
+
+    #print(type((row[1])))
